72c17a55e19332121d5aaaa8d198bfac273c3921
Anik <160725845+coderanik@users.noreply.github.com>
2026-03-10 22:27:55 +0530

    feat: Pass `setSelectedAnomaly` prop to `AnomalyMap` component.

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -61,6 +61,7 @@ export default function Dashboard() {
           anomalies={anomalies} 
           realPositions={realPositions}
           selectedAnomaly={selectedAnomaly}
+          setSelectedAnomaly={setSelectedAnomaly}
         />
       </div>
     </div>
